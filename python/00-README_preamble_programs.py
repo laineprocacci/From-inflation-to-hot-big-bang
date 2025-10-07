a65be7cde8c9c30b7d0ba64e62b5a7bfd6a41f5a
mikklaine <mxo100@gmail.com>
2025-10-07 08:31:47 +0200

    Update 00-README_preamble_programs.py

--- python/00-README_preamble_programs.py
+++ python/00-README_preamble_programs.py
@@ -3,7 +3,7 @@
 #
 # The python programs (*.py) have been tested with python3 on
 # a linux kernel, employing the numpy, scipy and sympy packages.
-# The mathematic scripts (*.m) have been tested with mathematica 4.0. 
+# The mathematic scripts (*.m) have been tested with mathematica 14.0. 
 #
 # Copyright (c) 2024-2025 Mikko Laine and Simona Procacci
 # 
@@ -12,5 +12,5 @@
 # No warranty is expressed or implied. 
 # 
 # For bug reports and other feedback, please contact
-# <laine@itp.unibe.ch> and <simona.procacci@unige.ch>.
+# <laine@itp.unibe.ch> and <simona.procacci@proton.me>.
 # 
